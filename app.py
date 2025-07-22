d5fde4a5b264997c48876d8ea6cfc50bff276e74
KarthigaSenthilkumar <karthigaibala.senthilkumar@gmail.com>
2025-07-22 20:53:42 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify, render_template
+import json
+import uuid
+
 
 app = Flask(__name__)
+
+users = {}
+user_id_counter = 1
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['Post'])
+@app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    message = request.form.get('message')
+    global user_id_counter
+    first_name = request.form.get('firstName')
+    last_name = request.form.get('lastName')
+    age = request.form.get('age')
+    gender = request.form.get('gender')
 
-    # Here you can add logic to save to database, send email, etc.
+    users[user_id_counter] = {
+        "id": str(uuid.uuid4())
 
-    return jsonify({
-        'status': 'success',
-        'name': name,
-        'email': email,
-        'message': message
-    })
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+        'gender': gender
+    }
+    user_id_counter += 1
+
+    return jsonify({'status': 'success', 'message': 'User added successfully'})
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    print(json.dumps(users))
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
